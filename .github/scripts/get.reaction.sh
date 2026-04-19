679180bd2c56212439c13ce01ca4738b557c8113
fridfn <117664140+fridfn@users.noreply.github.com>
2026-04-20 05:27:59 +0700

    update(major): add one new reaction data 'update' add mode logic at certain hours at any time

--- .github/scripts/get.reaction.sh
+++ .github/scripts/get.reaction.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-data=$(cat .github/messages/"$mode_message".json)
+data=$(cat .github/messages/reaction.json)
 
-# 1. pilih tone random dari mode
-tone=$(echo "$data" | jq -r --arg mode "$mode" '
-  .[$mode] | keys[]' | shuf -n 1)
+reaction=$(echo "$data" | jq -r \
+  --arg type "$type" \
+  --arg mode "$mode" \
+  '.[$type][$mode][]' | shuf -n 1)
 
-# 2. ambil greeting dari tone itu
-greeting=$(echo "$data" | jq -r --arg mode "$mode" --arg tone "$tone" '
-  .[$mode][$tone].greetings[]' | shuf -n 1)
 
-# 3. ambil message dari tone yang sama
-message=$(echo "$data" | jq -r --arg mode "$mode" --arg tone "$tone" '
-  .[$mode][$tone].messages[]' | shuf -n 1)
+# validasi
+if [ -z "$reaction" ] || [ "$reaction" = "null" ]; then
+  reaction="kamu tetap jalan hari ini… itu udah cukup"
+fi
 
-# 4. validasi
-if [ -z "$tone" ] || [ -z "$greeting" ] || [ -z "$message" ]; then
-  echo "No message found → exit"
+if [ -z "$reaction" ] || [ "$reaction" = "null" ]; then
+  echo "No reaction found → exit"
   exit 1
 fi
+
+echo "REACTION=$reaction"
